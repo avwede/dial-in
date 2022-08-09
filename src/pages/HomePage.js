fc81799a1b96eb108082623b5a2d49fc4ed3128e
avwede <avwede@gmail.com>
2022-08-09 14:43:41 -0400

    add favicon, file structure, etc.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.css";
+
+function HomePage() {
+    return (
+        <div className="background-img">
+            <img src="/images/espresso.jpg" alt="espresso" />
+        </div>
+    );
+}
+
+export default HomePage;
